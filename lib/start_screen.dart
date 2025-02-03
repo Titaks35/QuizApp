b4ffdd105e0434cb27f45162db09343ba48d5273
phuwanetsaksing6117 <phuwanetsaksing6117@gmail.com>
2025-02-03 23:50:59 +0000

    Add MaterialApp

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -6,41 +6,53 @@ class StartScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Image.asset(
-            'assets/images/quiz-logo.png',
-            width: 300,
-            color: Colors.white.withOpacity(0.6),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          const Text(
-            'Learn flutter the fun way!',
-            style: TextStyle(color: Colors.white, fontSize: 24),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          OutlinedButton.icon(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const QuestionsScreen(),
-                ),
-              );
-            },
-            icon: const Icon(Icons.arrow_right_alt, color: Colors.white),
-            label: const Text(
-              'Start Quiz',
-              style: TextStyle(color: Colors.white),
+    // Add MaterialApp to show the backgroudColor.
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.cyan, Colors.blue],
             ),
           ),
-        ],
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Image.asset(
+                  'assets/images/quiz-logo.png',
+                  width: 300,
+                  color: Colors.white.withOpacity(0.6),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                const Text(
+                  'Learn flutter the fun way!',
+                  style: TextStyle(color: Colors.white, fontSize: 24),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                OutlinedButton.icon(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const QuestionsScreen(),
+                      ),
+                    );
+                  },
+                  icon: const Icon(Icons.arrow_right_alt, color: Colors.white),
+                  label: const Text(
+                    'Start Quiz',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
