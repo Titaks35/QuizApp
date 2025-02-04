6acd17b758bd585029975a8b61f4cc987e2569b4
phuwanetsaksing6117 <phuwanetsaksing6117@gmail.com>
2025-02-04 16:05:42 +0000

    show the $totalMark and QuestionSummary()

--- lib/result_screen.dart
+++ lib/result_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:myapp/data/quizz.dart';
+import 'package:myapp/question_summary.dart';
 import 'package:myapp/start_screen.dart';
 
 class ResultScreen extends StatelessWidget {
@@ -10,13 +11,15 @@ class ResultScreen extends StatelessWidget {
 
   List<Map<String, Object>> getSummaryData() {
     List<Map<String, Object>> summaryData = [];
+
     for (int i = 0; i < selectedAnswers.length; i++) {
       bool isCorrect = selectedAnswers[i] == questions[i].answers[0];
+
       summaryData.add({
         'question_id': i,
         'question': questions[i].question,
-        'user_answer': selectedAnswers,
         'correct_answer': questions[i].answers[0],
+        'user_answer': selectedAnswers[i],
         'is_correct': isCorrect
       });
     }
@@ -25,6 +28,9 @@ class ResultScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final summary = getSummaryData();
+    final totalMark = summary.where((data) => data['is_correct'] as bool).length;
+    final totalQuestion = questions.length;
     // Copy from questions_screen.dart
     return MaterialApp(
       home: Scaffold(
@@ -35,57 +41,47 @@ class ResultScreen extends StatelessWidget {
             ),
           ),
           child: SizedBox(
-            child: Center(
-              child: Container(
-                margin: const EdgeInsets.all(40),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      "You answered 2 out of 6 questions correctly",
-                      style: GoogleFonts.lato(
-                        color: const Color.fromARGB(255, 6, 80, 184),
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
-                      textAlign: TextAlign.center,
+            child: Container(
+              margin: const EdgeInsets.all(40),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'You answered $totalMark out of $totalQuestion questions correctly',
+                    style: GoogleFonts.lato(
+                      color: const Color.fromARGB(255, 6, 80, 184),
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
                     ),
-                    const SizedBox(
-                      height: 30,
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  QuestionSummary(summary: getSummaryData()),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  TextButton.icon(
+                    icon: const Icon(
+                      Icons.restart_alt,
+                      color: Colors.white,
+                      size: 15,
                     ),
-                    Text(
-                      "You answered 2 out of 6 questions correctly",
-                      style: GoogleFonts.lato(
-                        color: const Color.fromARGB(255, 6, 80, 184),
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
-                      textAlign: TextAlign.center,
+                    label: const Text(
+                      'Restart Quiz',
+                      style: TextStyle(color: Colors.white, fontSize: 15),
                     ),
-                    const SizedBox(
-                      height: 30,
-                    ),
-                    TextButton.icon(
-                      icon: const Icon(
-                        Icons.restart_alt,
-                        color: Colors.white,
-                        size: 15,
-                      ),
-                      label: const Text(
-                        'Restart Quiz',
-                        style: TextStyle(color: Colors.white, fontSize: 15),
-                      ),
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => const StartScreen(),
-                          ),
-                        );
-                      },
-                    ),
-                  ],
-                ),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const StartScreen(),
+                        ),
+                      );
+                    },
+                  ),
+                ],
               ),
             ),
           ),
