bcf265277c9efd0f3796d4a6e29f4acfdc812ee5
phuwanetsaksing6117 <phuwanetsaksing6117@gmail.com>
2025-02-04 16:03:17 +0000

    start at StartScreen()

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:myapp/question_summary.dart';
-import 'package:myapp/questions_screen.dart';
-import 'package:myapp/result_screen.dart';
+//import 'package:myapp/question_summary.dart';
+//import 'package:myapp/questions_screen.dart';
+//import 'package:myapp/result_screen.dart';
 import 'package:myapp/start_screen.dart';
 
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
       body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            colors: [Colors.cyan, Colors.blue],
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.cyan, Colors.blue],
+            ),
           ),
-        ),
-        child: const QuestionSummary(summary: [])
-      ),
+          child: const StartScreen()),
     ),
   ));
 }
